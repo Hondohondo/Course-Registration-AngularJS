f2d172ef0f2dc018918d3f6045b594f8fb455a7e
hondohondo <mnandi19@jcu.edu>
2020-04-09 17:05:09 -0400

    CourseReg1 on CourseReg repo

--- app/js/controllers/CourseController.js
+++ app/js/controllers/CourseController.js
@@ -3,6 +3,11 @@
 courseApp.controller('CourseController',
   function CourseController ($scope) {
 
+    $scope.sortOrder = 'name';
+
+   // $scope.sortOrder = 'upVoteCount'; //ascending - low to high
+   // $scope.sortOrder = '-upVoteCount'; //descending - high to low
+
     $scope.course = {
       name: 'Angular Boot Camp',
       date: '7/7/2020',
@@ -17,7 +22,7 @@ courseApp.controller('CourseController',
         {
           name: 'Directives Masterclass',
           creatorName: 'Bob Smith',
-          duration: '1 hr',
+          duration: 1,
           level: 'Advanced',
           abstract: 'In this session you will learn the ins and outs of directive',
           upVoteCount: 0
@@ -25,7 +30,7 @@ courseApp.controller('CourseController',
         {
           name: 'Scopes for fun and profit',
           creatorName: 'John Doe',
-          duration: '30 mins',
+          duration: 2,
           level: 'Introductory',
           abstract: 'This session will take a closer look at scopes. Learn what they do, how they do it, and how to get them to do it for you',
           upVoteCount: 0
@@ -33,7 +38,7 @@ courseApp.controller('CourseController',
         {
           name: 'Well Behaved Controllers',
           creatorName: 'Jane Doe',
-          duration: '2 hours',
+          duration: 4,
           level: 'Intermediate',
           abstract: 'Controllers are the beginning of everything Angular does. Learn how to craft controllers that will win the respect of your friends and neighbors',
           upVoteCount: 0
--- web-server.js
+++ web-server.js
@@ -1,25 +1,29 @@
 var express = require('express');
-var path = require('path');
+// var path = require('path');
 var app = express();
-// var rootPath = path.normalize(__dirname + '/../');
-
-// app.use(express.static(rootPath + '/appcopy'));
-
-// var rootPath = path.normalize(__dirname + '/../');
-// app.use(express.static(rootPath + '/app'));
-// app.use(express.static('public'));
-
-// app.use(express.static(rootPath));
-
-// app.use(express.static(__dirname + 'app'));
-
-// app.use(express.static('app')); //this works...need to have an app folder
 
 app.use(express.static('app'));
 
-// app.use(express.static('../'));
-
-//app.use('/public', express.static('app')); //http://localhost:8000/public/img/app1.jpg //works
 
 app.listen(3000);
 console.log ('Listening on port 3000...Everything is okay');
+
+
+/*
+var rootPath = path.normalize(__dirname + '/../');
+
+app.use(express.static(rootPath + '/appcopy'));
+
+var rootPath = path.normalize(__dirname + '/../');
+app.use(express.static(rootPath + '/app'));
+app.use(express.static('public'));
+
+app.use(express.static(rootPath));
+
+app.use(express.static(__dirname + 'app'));
+
+app.use(express.static('app')); //this works...need to have an app folder
+
+app.use(express.static('../'));
+
+app.use('/public', express.static('app')); //http://localhost:8000/public/img/app1.jpg //works*/
